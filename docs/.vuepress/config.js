e7d9e3a0821c1948c2f4604cd6cfc0e24e9ca4fb
FIXxp <41650742+FIXxp@users.noreply.github.com>
2021-11-04 22:02:55 +0300

    Добавил метрику

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,6 +45,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['script', { type: 'text/javascript' }, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(86283189, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});'],
   ],
   plugins: [
     [
