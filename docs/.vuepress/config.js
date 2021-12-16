3fb10ecc02dbcc0e5a8d64f0a001d44cb74ed823
FIXxp <41650742+FIXxp@users.noreply.github.com>
2021-12-17 00:21:52 +0300

    По рофлу пихну

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,6 +47,8 @@ module.exports = {
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['script', { type: 'text/javascript', async: '' }, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(86283189, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',],
+    ['script', { type: 'text/javascript', async: '', src:'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4970334321972173',crossorigin:'anonymous',}]
+
   ],
   plugins: [
       '@vuepress/plugin-google-analytics',
