503eb74eb3db0aad15b383979e9b53200c607db0
FIXxp <41650742+FIXxp@users.noreply.github.com>
2022-01-15 01:49:48 +0300

    Ууу сука

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
   themeConfig: {
     lastUpdated: false,
     contributors: false,
-    logo: "/logo.png",
+    logo: "/logo.avif",
     navbar: [
       {
         text: "Discord",
@@ -58,7 +58,8 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#6b72f2' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#6b72f2' }],
     ['script', { type: 'text/javascript', async: '' }, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(86283189, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',],
   ],
   plugins: [
