b60b4212b8449e32d81b914952e677448db47740
FIXxp <41650742+FIXxp@users.noreply.github.com>
2022-03-22 09:36:30 +0300

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,7 +64,7 @@ module.exports = {
     ['script', { type: 'text/javascript', async: '' }, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(86283189, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});',],
   ],
   markdown:{
-    links: {externalAttrs: { target: '_blank' }}
+    links: {externalAttrs: { target: '_blank', rel: ''}}
   },
   plugins: [
     [
